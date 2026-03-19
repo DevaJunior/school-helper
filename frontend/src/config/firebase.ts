03e264668b0d31b9ca5e6f5e1ce7a44a3b804932
Spotted <spotted.contato.oficial@gmail.com>
2026-03-19 15:13:58 -0300

    <config> Firebase

---
+++ frontend/src/config/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAHtwKgYClG079F_KP2gDp4Ac7gXQiGQl4",
+  authDomain: "portifolio-d9c98.firebaseapp.com",
+  projectId: "portifolio-d9c98",
+  storageBucket: "portifolio-d9c98.firebasestorage.app",
+  messagingSenderId: "1077455395458",
+  appId: "1:1077455395458:web:8f0265f3d65311d97e729b"
+  //measurementId: "G-VQFWWK885T"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
